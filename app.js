d2ad2f96cf01950843c7dd887919bf515a640c39
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-03-02 21:14:35 +0530

    feat: Implement initial rover logic modules and configure Express app with middleware and a welcome route.

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app= express();
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status:'success',
+        message: 'Welcome to the API',
+    });
+});
