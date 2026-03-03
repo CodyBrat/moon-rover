9bb1b957eaff0a7af970fb559eaadb2e24d8b222
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-03-03 13:31:18 +0530

    feat: Enhance POST /api/rovers endpoint with rover processing logic and error handling

--- app.js
+++ app.js
@@ -25,9 +25,25 @@ app.post('/api/rovers',(req,res)=>{
         const parsed = parseInput(input);
         const results = [];
         const occupiedPositions = new Set();
-        
 
+        parsed.rovers.forEach(r=>occupiedPositions.add(`${r.x},${r.y}`))
+
+        for (rover of parsed.rovers){
+            occupiedPositions.delete(`${rover.x},${rover.y}`)
+            const result= runRover(rover,parsed.plateau, occupiedPositions)
+            results.push(`${result.x} ${result.y} ${result.facing}`)
+
+            occupiedPositions.add(`${result.x},${result.y}`)
+        }
+        return res.status(200).json({
+            status:'success',
+            data: results,
+        })
     }catch(error){
+        return res.status(400).json({
+            status:'fail',
+            message: error.message,
+        })
         
     }
 })
